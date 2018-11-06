543e6d359caf7629e321c3e52ad78a4a5980ae77
Alex Covert <acovert@ucsc.edu>
2018-11-05 19:50:44 -0800

    Player 2 camera rotates when left/right mouse button clicked

--- Assets/Scripts/CameraTwoRotator.cs
+++ Assets/Scripts/CameraTwoRotator.cs
@@ -5,34 +5,60 @@ using UnityEngine;
 public class CameraTwoRotator : MonoBehaviour {
 
     [SerializeField] private Camera playerTwoCam;
+    [SerializeField] private static int camPosHorizontal = 8;
+    [SerializeField] private static int camPosVertical = 3;
+    [SerializeField] private static int camRotationX = 15;
+    [SerializeField] private static int camRotationY = -45;
 
-    private Vector3 pos1 = new Vector3(8, 3, -8);
-    private Quaternion rot1 = Quaternion.Euler(15, -45, 0);
+    private Vector3 pos1 = new Vector3(camPosHorizontal, camPosVertical, -camPosHorizontal);
+    private Quaternion rot1 = Quaternion.Euler(camRotationX, camRotationY, 0);
 
-    private Vector3 pos2 = new Vector3(8, 3, 8);
-    private Quaternion rot2 = Quaternion.Euler(10, -135, 0);
+    private Vector3 pos2 = new Vector3(camPosHorizontal, camPosVertical, camPosHorizontal);
+    private Quaternion rot2 = Quaternion.Euler(camRotationX, camRotationY - 90, 0);
 
-    //private Vector3 pos3 = new Vector3(0, 2, 10);
-    //private Quaternion rot3 = Quaternion.Euler(10, 180, 0);
+    private Vector3 pos3 = new Vector3(-camPosHorizontal, camPosVertical, camPosHorizontal);
+    private Quaternion rot3 = Quaternion.Euler(camRotationX, camRotationY - 180, 0);
 
-    //private Vector3 pos4 = new Vector3(-10, 2, 0);
-    //private Quaternion rot4 = Quaternion.Euler(10, 90, 0);
+    private Vector3 pos4 = new Vector3(-camPosHorizontal, camPosVertical, -camPosHorizontal);
+    private Quaternion rot4 = Quaternion.Euler(camRotationX, camRotationY - 270, 0);
 
-    private string currentPos = "One";
+    private string currentPos;
 
     private void Start()
     {
         playerTwoCam.transform.position = pos1;
         playerTwoCam.transform.rotation = rot1;
+        currentPos = "One";
     }
 
     private void Update()
     {
+        Debug.Log(currentPos);
         if(Input.GetMouseButtonDown(0))
         {
-            if(currentPos == "One")
+            if (currentPos == "One")
             {
-
+                playerTwoCam.transform.position = pos4;
+                playerTwoCam.transform.rotation = rot4;
+                currentPos = "Four";
+            }
+            else if (currentPos == "Two")
+            {
+                playerTwoCam.transform.position = pos1;
+                playerTwoCam.transform.rotation = rot1;
+                currentPos = "One";
+            }
+            else if (currentPos == "Three")
+            {
+                playerTwoCam.transform.position = pos2;
+                playerTwoCam.transform.rotation = rot2;
+                currentPos = "Two";
+            }
+            else if (currentPos == "Four")
+            {
+                playerTwoCam.transform.position = pos3;
+                playerTwoCam.transform.rotation = rot3;
+                currentPos = "Three";
             }
         }
         if(Input.GetMouseButtonDown(1))
@@ -41,6 +67,25 @@ public class CameraTwoRotator : MonoBehaviour {
             {
                 playerTwoCam.transform.position = pos2;
                 playerTwoCam.transform.rotation = rot2;
+                currentPos = "Two";
+            }
+            else if (currentPos == "Two")
+            {
+                playerTwoCam.transform.position = pos3;
+                playerTwoCam.transform.rotation = rot3;
+                currentPos = "Three";
+            }
+            else if (currentPos == "Three")
+            {
+                playerTwoCam.transform.position = pos4;
+                playerTwoCam.transform.rotation = rot4;
+                currentPos = "Four";
+            }
+            else if (currentPos == "Four")
+            {
+                playerTwoCam.transform.position = pos1;
+                playerTwoCam.transform.rotation = rot1;
+                currentPos = "One";
             }
         }
     }
