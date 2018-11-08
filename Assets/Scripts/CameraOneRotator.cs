917462b1083af94c0e371ac8f74fc980a79582d7
Alex Covert <acovert@ucsc.edu>
2018-11-08 14:37:32 -0800

    Switch positions and rotations in camera one to an array

--- Assets/Scripts/CameraOneRotator.cs
+++ Assets/Scripts/CameraOneRotator.cs
@@ -6,25 +6,35 @@ public class CameraOneRotator : MonoBehaviour {
 
     [SerializeField] private Camera playerOneCam;
     [SerializeField] private float moveSpeed = 30;
+
     //Change these if the tower size changes
     private static int camPosHorizontal = 22;
-    private static int camPosVertical = 5;
-    private static int camRotationX = 10;
-    private static int camRotationY = 0;
+    private static int camPosVertical   = 5;
+    private static int camRotationX     = 10;
+    private static int camRotationY     = 0;
 
+    private Vector3[] positions = new[] {  new Vector3(0, camPosVertical, -camPosHorizontal),
+                                           new Vector3(camPosHorizontal, camPosVertical, 0),
+                                           new Vector3(0, camPosVertical, camPosHorizontal),
+                                           new Vector3(-camPosHorizontal, camPosVertical, 0)};
+
+    private Quaternion[] rotations = new[] { Quaternion.Euler(camRotationX, camRotationY, 0),
+                                             Quaternion.Euler(camRotationX, camRotationY - 90, 0),
+                                             Quaternion.Euler(camRotationX, camRotationY - 180, 0),
+                                             Quaternion.Euler(camRotationX, camRotationY - 270, 0)};
 
     //Create positions and rotations from given defaults
-    private Vector3 pos1 = new Vector3(0, camPosVertical, -camPosHorizontal);
-    private Quaternion rot1 = Quaternion.Euler(camRotationX, camRotationY, 0);
+    //private Vector3 pos1 = new Vector3(0, camPosVertical, -camPosHorizontal);
+    //private Quaternion rot1 = Quaternion.Euler(camRotationX, camRotationY, 0);
 
-    private Vector3 pos2 = new Vector3(camPosHorizontal, camPosVertical, 0);
-    private Quaternion rot2 = Quaternion.Euler(camRotationX, camRotationY - 90, 0);
+    //private Vector3 pos2 = new Vector3(camPosHorizontal, camPosVertical, 0);
+    //private Quaternion rot2 = Quaternion.Euler(camRotationX, camRotationY - 90, 0);
 
-    private Vector3 pos3 = new Vector3(0, camPosVertical, camPosHorizontal);
-    private Quaternion rot3 = Quaternion.Euler(camRotationX, camRotationY + 180, 0);
+    //private Vector3 pos3 = new Vector3(0, camPosVertical, camPosHorizontal);
+    //private Quaternion rot3 = Quaternion.Euler(camRotationX, camRotationY + 180, 0);
 
-    private Vector3 pos4 = new Vector3(-camPosHorizontal, camPosVertical, 0);
-    private Quaternion rot4 = Quaternion.Euler(camRotationX, camRotationY + 90, 0);
+    //private Vector3 pos4 = new Vector3(-camPosHorizontal, camPosVertical, 0);
+    //private Quaternion rot4 = Quaternion.Euler(camRotationX, camRotationY + 90, 0);
 
     private float distance, covered, remaining;
     private float startTime;
@@ -36,11 +46,11 @@ public class CameraOneRotator : MonoBehaviour {
 
     private void Start()
     {
-        playerOneCam.transform.position = pos1;
-        playerOneCam.transform.rotation = rot1;
+        playerOneCam.transform.position = positions[0];
+        playerOneCam.transform.rotation = rotations[0];
         cameraState = 1;
 
-        distance = Vector3.Distance(pos1, pos2);
+        distance = Vector3.Distance(positions[0], positions[1]);
 
         moving = false;
     }
@@ -67,46 +77,46 @@ public class CameraOneRotator : MonoBehaviour {
     {
         if(other.tag == "Trigger1")
         { 
-            distance = Vector3.Distance(pos1, pos2);
+            distance = Vector3.Distance(positions[0], positions[1]);
             startTime = Time.time;
             moving = true;
-            prevPosition = pos1;
-            goalPosition = pos2;
-            prevRotation = rot1;
-            goalRotation = rot2;
+            prevPosition = positions[0];
+            goalPosition = positions[1];
+            prevRotation = rotations[0];
+            goalRotation = rotations[1];
             cameraState = 2;
         }
         if (other.tag == "Trigger2")
         {
-            distance = Vector3.Distance(pos2, pos3);
+            distance = Vector3.Distance(positions[1], positions[2]);
             startTime = Time.time;
             moving = true;
-            prevPosition = pos2;
-            goalPosition = pos3;
-            prevRotation = rot2;
-            goalRotation = rot3;
+            prevPosition = positions[1];
+            goalPosition = positions[2];
+            prevRotation = rotations[1];
+            goalRotation = rotations[2];
             cameraState = 3;
         }
         if (other.tag == "Trigger3")
         { 
-            distance = Vector3.Distance(pos3, pos4);
+            distance = Vector3.Distance(positions[2], positions[3]);
             startTime = Time.time;
             moving = true;
-            prevPosition = pos3;
-            goalPosition = pos4;
-            prevRotation = rot3;
-            goalRotation = rot4;
+            prevPosition = positions[2];
+            goalPosition = positions[3];
+            prevRotation = rotations[2];
+            goalRotation = rotations[3];
             cameraState = 4;
         }
         if (other.tag == "Trigger4")
         {
-            distance = Vector3.Distance(pos4, pos1);
+            distance = Vector3.Distance(positions[3], positions[0]);
             startTime = Time.time;
             moving = true;
-            prevPosition = pos4;
-            goalPosition = pos1;
-            prevRotation = rot4;
-            goalRotation = rot1;
+            prevPosition = positions[3];
+            goalPosition = positions[0];
+            prevRotation = rotations[3];
+            goalRotation = rotations[0];
             cameraState = 1;
         }
     }
