98b6984d751d3bd515447f4b08850baeff1c5860
Alex Covert <acovert@ucsc.edu>
2018-11-24 13:34:11 -0800

    Change placing traps to be more gridlike

--- Assets/Scripts/PlayerTwo.cs
+++ Assets/Scripts/PlayerTwo.cs
@@ -11,6 +11,8 @@ public class PlayerTwo : MonoBehaviour {
     [SerializeField] private Camera cam;
     [SerializeField] private float widthBetweenTraps;
     [SerializeField] private float heightBetweenTraps;
+    [SerializeField] private int horizontalGridSize;
+    [SerializeField] private int verticalGridSize; 
 
     private GameObject trap;
     private GameObject ghostTrap;
@@ -45,14 +47,18 @@ public class PlayerTwo : MonoBehaviour {
 
         if (Physics.Raycast(ray, out hit, 99999, ~LayerMask.NameToLayer("Tower")))
         {
-            Vector3 hitPos = hit.point + hit.normal * 5;
+            int hitX = Mathf.RoundToInt(hit.point.x / horizontalGridSize) * horizontalGridSize;
+            int hitZ = Mathf.RoundToInt(hit.point.z / horizontalGridSize) * horizontalGridSize;
+            int hitY = Mathf.RoundToInt(hit.point.y / verticalGridSize) * verticalGridSize;
+
+
+            Vector3 hitPos = new Vector3(hitX, hitY, hitZ) + hit.normal * 5;
             Quaternion hitRot = Quaternion.identity;
 
             if (hit.normal.x == -1 || hit.normal.x == 1)
             {
                 hitRot = Quaternion.Euler(0, 90, 0);
             }
-
 
 
             if (ghostTrap != null)
@@ -65,10 +71,12 @@ public class PlayerTwo : MonoBehaviour {
             {
                 Instantiate(trap, hitPos, hitRot);
                 trap = null;
-                Destroy(ghostTrap);
-                ghostTrap = null;
+                Cursor.visible = true;
+                DestroyGhost();
             }
         }
+        else
+            Cursor.visible = true;
     }
 
     //Check for nearby platforms/traps to see if it is too close to place a new one.
@@ -89,30 +97,6 @@ public class PlayerTwo : MonoBehaviour {
         return true;
     }
 
-    private void OnClickTrap1()
-    {
-        trap = traps[0];
-        SetGhost();
-    }
-
-    private void OnClickTrap2()
-    {
-        trap = traps[1];
-        SetGhost();
-    }
-
-    private void OnClickTrap3()
-    {
-        trap = traps[2];
-        SetGhost();
-    }
-
-    private void OnClickTrap4()
-    {
-        trap = traps[3];
-        SetGhost();
-    }
-
     private void SetGhost()
     {
         if(trap != null)
@@ -122,6 +106,57 @@ public class PlayerTwo : MonoBehaviour {
             color.a = 0.5f;
             ghostTrap.GetComponent<MeshRenderer>().material.color = color;
             ghostTrap.tag = "Untagged";
+
+            Cursor.visible = false;
         }
+    }
+
+
+    private void DestroyGhost()
+    {
+        if(ghostTrap != null)
+        {
+            Destroy(ghostTrap);
+            ghostTrap = null;
+        }
+    }
+
+    //Set cursor visible when hovering over a button
+    public void OnPointerEnterSetCursor()
+    {
+        Cursor.visible = true;
+    }
+
+    public void OnPointerExitSetCursor()
+    {
+        Cursor.visible = false;
+    }
+
+    private void OnClickTrap1()
+    {
+        trap = traps[0];
+        DestroyGhost();
+        SetGhost();
+    }
+
+    private void OnClickTrap2()
+    {
+        trap = traps[1];
+        DestroyGhost();
+        SetGhost();
+    }
+
+    private void OnClickTrap3()
+    {
+        trap = traps[2];
+        DestroyGhost();
+        SetGhost();
+    }
+
+    private void OnClickTrap4()
+    {
+        trap = traps[3];
+        DestroyGhost();
+        SetGhost();
     }
 }
