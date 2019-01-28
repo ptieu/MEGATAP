18940536f6ea3b0c12edfcbe1869f261ce2ffc3b
Edward Li <edjli@ucsc.edu>
2019-01-28 15:49:29 -0800

    added stun/havent checked functionality

--- Assets/Scripts/Traps/TrapBase.cs
+++ Assets/Scripts/Traps/TrapBase.cs
@@ -8,9 +8,9 @@ public class TrapBase : MonoBehaviour {
     // apply knockback to inputted
     // must be used in a FixedUpdate method, will apply velocity per frame. Use a timing
     // method to decide how many frames force is applied.
-    public void KnockBack(GameObject obj, int knockBackDistance, int knockUpDistance) 
+    public void KnockBack(GameObject obj, int knockBackDistance, int knockUpDistance)
     {
-        // find out which way the player is facing so the knock can 
+        // find out which way the player is facing so the knock can
         int playerDirection = obj.gameObject.GetComponent<PlayerOneMovement>().GetState();
         Rigidbody rb = obj.gameObject.GetComponent<Rigidbody>();
         rb.AddForce(transform.forward * -1 * knockBackDistance);
@@ -32,9 +32,17 @@ public class TrapBase : MonoBehaviour {
     }
 
     // apply knockback to inputted
-    public void Stun(Collision obj, int stunDuration)
+    public void Stun(GameObject player, Collision obj, int stunDuration)
     {
-        Debug.Log("Stun");
+      Rigidbody rb = player.gameObject.GetComponent<Rigidbody>();
+      if (obj.gameObject.tag == "Player"){
+        rb.velocity = new Vector3(0,0,0);
+        StartCoroutine(DoStun(stunDuration));
+      }
+    }
+
+    IEnumerator DoStun(int stunDuration){
+      yield return new WaitForSeconds(stunDuration);
     }
 
     // apply knockback to inputted
