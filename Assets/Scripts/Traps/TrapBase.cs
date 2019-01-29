85555a728fc8edd92ed1eeec5c1ae44fe570c56d
Trevor Jordy <tjordy@ucsc.edu>
2019-01-28 17:56:13 -0800

    huge changes to knockback mechanic

--- Assets/Scripts/Traps/TrapBase.cs
+++ Assets/Scripts/Traps/TrapBase.cs
@@ -5,30 +5,68 @@ using UnityEngine;
 // a base class for traps to build on
 public class TrapBase : MonoBehaviour {
 
+    // keeps track of which direction the player was moving at the moment of this save
+    // be sure to call UpdatePlayerVelocities() before using these variables
+    private int playerx = 0;
+    private int playery = 0;
+    private int playerz = 0;
+
+    // call before using the playerx playery playerz variables
+    public void UpdatePlayerVelocities( GameObject obj )
+    {
+        // find out the velocity of the player
+        if (obj.gameObject.GetComponent<Rigidbody>().velocity.x > 0)
+        {
+            playerx = 1;
+        }
+        else if (obj.gameObject.GetComponent<Rigidbody>().velocity.x < 0)
+        {
+            playerx = -1;
+        }
+        if (obj.gameObject.GetComponent<Rigidbody>().velocity.y > 0)
+        {
+            playery = 1;
+        }
+        else if (obj.gameObject.GetComponent<Rigidbody>().velocity.y < 0)
+        {
+            playery = -1;
+        }
+        if (obj.gameObject.GetComponent<Rigidbody>().velocity.z > 0)
+        {
+            playerz = 1;
+        }
+        else if (obj.gameObject.GetComponent<Rigidbody>().velocity.z < 0)
+        {
+            playerz = -1;
+        }
+    }
+
     // apply knockback to inputted
     // must be used in a FixedUpdate method, will apply velocity per frame. Use a timing
     // method to decide how many frames force is applied.
     public void KnockBack(GameObject obj, int knockBackDistance, int knockUpDistance) 
     {
-        // find out which way the player is facing so the knock can 
+        // find out which way the player is facing (on faces of the tower) so the knock can do accordingly
         int playerDirection = obj.gameObject.GetComponent<PlayerOneMovement>().GetState();
         Rigidbody rb = obj.gameObject.GetComponent<Rigidbody>();
         rb.AddForce(transform.forward * -1 * knockBackDistance);
+
         switch (playerDirection)
         {
             case 1:
-                rb.velocity = new Vector3(-knockBackDistance, knockUpDistance, 0);
+                rb.velocity = new Vector3(-knockBackDistance * playerx, knockUpDistance * -playery, 0);
                 break;
             case 2:
-                rb.velocity = new Vector3(0, knockUpDistance, -knockBackDistance);
+                rb.velocity = new Vector3(0, knockUpDistance * -playery, -knockBackDistance * playerz);
                 break;
             case 3:
-                rb.velocity = new Vector3(knockBackDistance, knockUpDistance, 0);
+                rb.velocity = new Vector3(knockBackDistance * -playerx, knockUpDistance * playery, 0);
                 break;
             case 4:
-                rb.velocity = new Vector3(0, knockUpDistance, knockBackDistance);
+                rb.velocity = new Vector3(0, knockUpDistance * -playery, knockBackDistance * playerz);
                 break;
         }
+        Debug.Log(playerx + " " + playery + " " + playerz);
     }
 
     // apply knockback to inputted
