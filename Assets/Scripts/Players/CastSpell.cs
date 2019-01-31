421bf30da3518121a53a00c5c9b7b0d32c2ab572
Alex Covert <acovert@ucsc.edu>
2019-01-31 13:39:46 -0800

    Start out castspell code

---
+++ Assets/Scripts/Players/CastSpell.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CastSpell : MonoBehaviour {
+    [SerializeField] private GameManager gm;
+    [SerializeField] private Image controllerCursor;
+
+	
+	void Update () {
+        bool controller = gm.GetControllerTwoState();
+        float cursorX, cursorY;
+        if (controller)
+        {
+            cursorX = controllerCursor.transform.position.x;
+            cursorY = controllerCursor.transform.position.y;
+        }
+        else
+        {
+            cursorX = Input.mousePosition.x;
+            cursorY = Input.mousePosition.y;
+        }
+
+        //Get rid of cursorY != 0
+        if(cursorX < Screen.width / 2 && cursorY != 0)
+        {
+            //Debug.Log(cursorX + ", " + cursorY);
+        }
+	}
+}
